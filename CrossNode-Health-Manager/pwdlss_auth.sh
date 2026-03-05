b611c485445b59f2493e57e3ea19cec6b23d8b16
Ozair Khan <ozairk048@gmail.com>
2026-03-05 23:52:35 +0500

    Update comments in pwdlss_auth.sh for clarity
    Added notes about key location and usage for pwdlss_auth.sh.

--- CrossNode-Health-Manager/pwdlss_auth.sh
+++ CrossNode-Health-Manager/pwdlss_auth.sh
@@ -2,6 +2,10 @@
 # When the instances were created using Terrafrom. 
 # Althouh for private .pem file it works fine no matter where the key is present
 
+# So The best Alternative use to this program is a modified version with name "Prod_Rdy_PwdLess_Aut.sh" if the key is at default location. the program detects the key 
+# Automatically. If the key is not at the default location it is recommended to pass the key while running the program. 
+# Example: bash Prod_Rdy_PwdLess_Aut.sh /home/ubuntu/projectAnsible/VmHealth_Project/testKey.pem
+
 
 #!/bin/bash
 
